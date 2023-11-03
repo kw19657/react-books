216b576d411378c2cef193a1ce7a285474a69c1f
kw19657 <106737706+kw19657@users.noreply.github.com>
2023-11-03 14:52:31 +0800

    installed axios and wrote api calls

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,23 +1,32 @@
 import { useState } from 'react';
 import BookEdit from './BookEdit';
 
-const BookShow = ({book, onDelete}) => {
+const BookShow = ({book, onDelete, onEdit}) => {
   const [showEdit, setShowEdit] = useState(false);
 
   const handleEditClick = () => {
     setShowEdit(!showEdit);
   }
 
+  const handleSubmit = (id, newTitle) => {
+    setShowEdit(false);
+    onEdit(id, newTitle);
+  }
+
   let content = <h3>{book.title}</h3>;
 
   if (showEdit) {
-    content = <BookEdit />;
+    content = <BookEdit onSubmit={handleSubmit} book={book}/>;
   }
 
   return (
     <div className="book-show">
+      <img
+        alt="books"
+        src={`https://picsum.photos/seed/${book.id}/200`}
+      />
       <div>{content}</div>
-      {book.title}
+      {/* {book.title} */}
       <div className="actions">
         <button className="edit" onClick={handleEditClick}>
           Edit
