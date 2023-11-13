2913297b6b3b91154342c66023b89ce2c2b2a8d5
kw19657 <106737706+kw19657@users.noreply.github.com>
2023-11-13 19:55:58 +0800

    updated booklist

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,7 +2,7 @@ import BookShow from './BookShow';
 import useBooksContext from '../hooks/use-books-context';
 
 const BookList = () => {
-  const { books } = useBooksContext();
+  const { books } = useBooksContext()
 
   const renderedBooks = books.map((book) => {
     return <BookShow key={book.id} book={book} />
