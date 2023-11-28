5f3a75f1bcec1aa8ad982375f20f4c54701c98c2
kw19657 <106737706+kw19657@users.noreply.github.com>
2023-11-28 13:58:52 +0800

    bookmap

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,6 +4,7 @@ import useBooksContext from '../hooks/use-books-context';
 const BookList = () => {
   const { books } = useBooksContext()
 
+  // maps the list of books to screen
   const renderedBooks = books.map((book) => {
     return <BookShow key={book.id} book={book} />
   })
